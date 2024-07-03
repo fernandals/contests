46e01025813bcabcf4b1014df288fd651830f276
Fernanda Lustosa <lustosafk@gmail.com>
2024-07-03 12:23:56 -0300

    solucao genial eudiria

--- contest-10/c-computerGame.cpp
+++ contest-10/c-computerGame.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+vector<pair<int, int>> neighborhood = {make_pair(-1,-1), make_pair(-1, 0), make_pair(-1, 1), make_pair(0, -1), make_pair(0, 1), make_pair(1, -1), make_pair(1, 0), make_pair(1, 1)};
+
+int conv_index(int n, int i, int j) {
+  if(i == 0)
+    return j+1;
+  else
+    return j+n+1;
+}
+
 int main () {
   int t;
   cin >> t;
@@ -11,29 +20,55 @@ int main () {
     cin >> n;
 
     int grid[2][n];
+    char num;
     for(int i=0; i<2; i++)
-      for(int j=0; j<n; j++)
-	cin >> grid[i][j];
-
-    unordered_map<int, pair<int, int>> conv_index;
-    for(int idx=1; idx<=2*n; idx++) {
-      if(idx <= n)
- 	conv_index[idx] = make_pair(1, idx);
-      else {
-	int new_idx = idx%n == 0 ? n : idx%n;
-	conv_index[idx] = make_pair(2, new_idx);
+      for(int j=0; j<n; j++) {
+	cin >> num;
+	grid[i][j] = num - '0';
       }
-    }
 
-    vector<vector<int>> adj(n+1,vector<int>(n+1, 0));
+    vector<vector<int>> adj(2*n+1,vector<int>(2*n+1, 0));
     for(int i=0; i<2; i++) {
       for(int j=0; j<n; j++) {
-	if()
+	// se é uma posição valida olha os vizinhos	
+	if(grid[i][j] == 0)
+	  for(auto neighbor : neighborhood) {
+	    int row_n = i + neighbor.first;
+	    int col_n = j + neighbor.second;
 
-	// checar se ao redor ha casas com 0
-	// pra se sim ent ela vira uma nova aresta no grafo
+	    if(row_n >= 0 and row_n < 2 and col_n >= 0 and col_n < n)
+	      if(grid[row_n][col_n] == 0) {
+		int idx_u = conv_index(n, i, j);
+		int idx_v = conv_index(n, row_n, col_n);
+
+		adj[idx_u][idx_v] = adj[idx_v][idx_u] = 1;
+	      }
+	  }
       }
     }
+
+    queue<int> path;
+    vector<bool> visited(2*n+1, false);
+    
+    visited[0] = visited[1] = true;
+    path.push(1);
+
+    while(!path.empty()) {
+      int k = path.front();
+      path.pop();
+
+      for(int i = 1; i <= 2*n; i++) {
+	if(adj[k][i] == 1 and !visited[i]) {
+	  visited[i] = true;
+	  path.push(i);
+	}
+      }
+    }
+
+    if(visited[2*n])
+      cout << "YES" << endl;
+    else
+      cout << "NO" << endl;
   }
 
   return 0;
